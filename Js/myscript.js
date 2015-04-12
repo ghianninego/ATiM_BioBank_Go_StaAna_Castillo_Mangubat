54b561ac48d6b0946a304e594346f59560805a1a
ghianninego <Ghiannineee@Ghiannines-MacBook-Air.local>
2015-04-12 16:58:33 +0800

    Separated the .js file from html

---
+++ Js/myscript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$("#choice1").click(function(){
+		$("#tab1").slideToggle("slow");
+	});
+					
+	$("#choice2").click(function(){
+		$("#tab2").slideToggle("slow");
+	});
+					
+	$("#choice3").click(function(){
+		$("#tab3").slideToggle("slow");
+	});
+});
