4da54f1aedd78dfd9e2b3c13538673078f736605
ghianninego <gsjg_17@yahoo.com>
2015-04-25 23:53:24 +0800

    Delete myscript.js

--- Js/myscript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
-});
-
-$('myTab a').click(function(e) {
-	e.preventDefault()
-	$(this).tab('show')
-})
