a0246e21b2b59a68ef33859ce041a9401ceb0120
ghianninego <gsjg_17@yahoo.com>
2015-04-25 23:43:17 +0800

    edited!

--- Js/myscript.js
+++ Js/myscript.js
@@ -1,20 +1,8 @@
-function logout(){
-	var x=confirm("Are you sure you want to logout?");
-	if (x) {
-		location.assign("../Html/index.html");
-	}
-}
-
 $(document).ready(function(){
-	$("#choice1").click(function(){
-		$("#tab1").slideToggle("slow");
-	});
-					
-	$("#choice2").click(function(){
-		$("#tab2").slideToggle("slow");
-	});
-					
-	$("#choice3").click(function(){
-		$("#tab3").slideToggle("slow");
-	});
+	$("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
 });
+
+$('myTab a').click(function(e) {
+	e.preventDefault()
+	$(this).tab('show')
+})
