7449a2c6db6de11f4f953a23c384a8ecaf4e6e4e
ghianninego <Ghiannineee@Ghiannines-MacBook-Air.local>
2015-04-26 14:57:00 +0800

    Major Editting lol
    Use ‘user’ as username and password to login as a regular user.
    
    Use ‘admin’ as username and password to login as an administrator.

--- js/myscript.js
+++ js/myscript.js
@@ -6,3 +6,17 @@ $('myTab a').click(function(e) {
 	e.preventDefault()
 	$(this).tab('show')
 })
+
+$(function(){
+    $('#log').click(function(){
+    	var nameVal = document.getElementById("username").value;
+    	var passVal = document.getElementById("pwd").value;
+        
+    	if (nameVal == "user" && passVal== "user") {
+    		window.location = '../html/profile-user.html';
+		}
+    	else if (nameVal == "admin" && passVal== "admin") {
+    		window.location = '../html/profile-admin.html';
+    	}
+    });
+});
