f626a721f1a426b53ecb0aea6924128b166d039d
ghianninego <Ghiannineee@Ghiannines-MacBook-Air.local>
2015-05-25 15:42:09 +0800

    edits

--- js/myscript.js
+++ js/myscript.js
@@ -9,7 +9,19 @@ $('myTab a').click(function(e) {
 })
 
 $(function(){
-    $('#log').click(function(){
+    $('#logbtn').click(function(){
+    	var nameVal = document.getElementById("username").value;
+    	var passVal = document.getElementById("pwd").value;
+        
+    	if (nameVal == "user" && passVal== "user") {
+    		window.location = 'html/profile-user.html';
+		}
+    	else if (nameVal == "admin" && passVal== "admin") {
+    		window.location = 'html/profile-admin.html';
+    	}
+    });
+
+    $('#logbtn1').click(function(){
     	var nameVal = document.getElementById("username").value;
     	var passVal = document.getElementById("pwd").value;
         
@@ -20,4 +32,12 @@ $(function(){
     		window.location = '../html/profile-admin.html';
     	}
     });
+
+	$('#reg_user').click(function(){
+		$('#form_user').submit();
+	});
+	
+	$('#reg_admin').click(function(){
+		$('#form_admin').submit();
+	});
 });
